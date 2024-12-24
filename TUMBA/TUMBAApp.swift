cc216039397e93e8adbd2bd4c6e176194728d661
IrinaApolonnik <iaapolonnik@edu.hse.ru>
2024-12-25 02:53:24 +0300

    Initial Commit

---
+++ TUMBA/TUMBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUMBAApp.swift
+//  TUMBA
+//
+//  Created by Евгения Аполонник on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TUMBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TUMBATests/TUMBATests.swift
@@ -0,0 +1,17 @@
+//
+//  TUMBATests.swift
+//  TUMBATests
+//
+//  Created by Евгения Аполонник on 25.12.2024.
+//
+
+import Testing
+@testable import TUMBA
+
+struct TUMBATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
